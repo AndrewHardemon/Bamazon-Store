97d6b9b738e4de8d997482e41fdb59c1ffab07a3
Andrew Hardemon <aeroatlas@ymail.com>
2020-01-10 21:39:55 -0700

    almost got it working

--- bamazon.sql
+++ bamazon.sql
@@ -14,8 +14,7 @@ CREATE TABLE products (
   PRIMARY KEY (id)
 )
 
---SEEDS
-INSERT INTO top1000db (item_id, product_name, department_name, price, stock_quality)
+INSERT INTO products (item_id, product_name, department_name, price, stock_quality)
 VALUES (1111, "Bananas", "Food Feeder", 2.10, 50),
 (2352, "Robot Baby", "RobotLand", 222.22, 10),
 (9764, "Wedding Ring", "Chain 'em n' Drain 'em", 1000.00, 5),
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,50 +11,48 @@ var connection = mysql.createConnection({
   database: "productsdb"
 });
 
+connection.connect(function(err) {
+  if (err) throw err;
+  menuCustomer()
+});
 
-// * View Products for Sale
-    
-// * View Low Inventory
-
-// * Add to Inventory
-
-// * Add New Product
-
+//Menu
 function menuCustomer(){
   inquirer.prompt([
     {
       type: "list",
       name: "menu",
       message: "Menu Options",
-      choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+      choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"]
     }
   ]).then(function(res) {
-
-    connection.connect(function(err) {
-      if (err) throw err;
-      console.log("connected as id " + connection.threadId);
-
-      //Which was picked
-      if (res.menu === "View Products for Sale") {
-        console.log("Products");
-        afterConnection();
-        menuCustomer();
-      }
-      if (res.menu === "View Low Inventory") {
-        console.log("Products lower than 5")
-        lowConnection();
-        menuCustomer();
-      }
-      if (res.menu === "Add to Inventory") {
-        console.log("this works")
-
-      }
-      if (res.menu === "Add New Product") {
-        console.log("this works")
-        inquirerAdd();
-        addProduct();
-      }
-    });
+    //Which was picked
+    if (res.menu === "View Products for Sale") {
+      console.log("Products");
+      afterConnection();
+      menuCustomer();
+    }
+    else if (res.menu === "View Low Inventory") {
+      console.log("Products lower than 5")
+      lowConnection();
+      menuCustomer();
+    }
+    else if (res.menu === "Add to Inventory") {
+      console.log("this works")
+      updateProduct()
+    }
+    else if (res.menu === "Add New Product") {
+      console.log("this works")
+      inquirerAdd();
+      addProduct();
+    }
+    else if (res.menu === "Exit") {
+      console.log("this works")
+      connection.end();
+    }
+    else {
+      menuCustomer();
+    }
   });
 }
 
@@ -62,11 +60,39 @@ function menuCustomer(){
 function afterConnection() {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
-    //console.log(res);
+    console.log(res);
     //connection.end();
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "Give the ID of the product you want",
+        name: "name"
+      },
+      {
+        type: "input",
+        message: "How many of the product do you want",
+        name: "num"
+      }
+    ]).then(function(res) {
+        buyProduct(res.name, res.num);
+    })
   });
 }
 
+//Buy the Product
+function buyProduct(name, num){
+  connection.query("SELECT * FROM products WHERE item_id = ?", [name], function(err, res){
+  if (err) throw err;
+  if(res.stock_quality === 0){
+    console.log("Insuffecient Funds")
+  } else {
+    console.log(res.price)
+    updateProduct(name, -1)
+  }
+  })
+}
+
+//Check for low stock products
 function lowConnection() {
   connection.query("SELECT * FROM products WHERE stock_quality <= 5", function(err, res) {
     if (err) throw err;
@@ -75,26 +101,35 @@ function lowConnection() {
   });
 }
 
-//Adds Products with MySQL
-function addProduct() {
-  console.log("Inserting a new product...\n");
-  var query = connection.query(
-    "INSERT INTO songs SET ?",
-    {
-      artist: "Gorgoroth",
-      title: "Satan Prometheus",
-      genre: "Black Metal"
-    }, function(err, res){
-      if (err) throw err;
-      console.log(` ${res.affectedRows} song inserted!`)
-      updateDB();
-    }
-  )
-  console.log(query.sql);
-
+//Add more stock to a product
+function updateProduct(name, addSub) {
+  console.log(`Updating all genre of songs titled Eaten...\n`);
+  //connection.query("SQL STRING", arguments, callback()) //This is the order of operations
+  connection.query("SELECT * FROM products WHERE item_id = ?", [name], function(err, res) {
+    if (err) throw err;
+    var nuNum = res.stock_quality + num
+    var query = connection.query(
+      "UPDATE products SET ? WHERE ?",
+      [
+        {
+          quantity: 100
+        },
+        {
+          item_id: name
+        }
+      ],
+      function(err, res) {
+        if (err) throw err;
+        console.log(res.affectedRows + " products updated!\n");
+        // Call deleteProduct AFTER the UPDATE completes
+        deleteProduct();
+      });
+  });
 }
 
-//Helps add new product using inquirer
+
+
+//Helps add new product using inquirer (Part 1)
 function inquirerAdd(){
   inquirer.prompt([
     {
@@ -121,6 +156,26 @@ function inquirerAdd(){
       type: "input",
       message: "How many would you like to add?",
       name: "stock_quality"
-    },
-  ]).then(function(res) {})
+    }
+  ]).then(function(res) {
+      addProduct(res);
+  })
+}
+
+//Adds Products with MySQL (Part 2)
+function addProduct() {
+  console.log("Inserting a new product...\n");
+  var query = connection.query(
+    "INSERT INTO products SET ?",
+    {
+      artist: res.price,
+      title: "Satan Prometheus",
+      genre: "Black Metal"
+    }, function(err, res){
+      if (err) throw err;
+      console.log(` ${res.affectedRows} song inserted!`)
+      updateDB();
+    }
+  )
+  console.log(query.sql);
 }
